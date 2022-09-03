ae6d9dfdfa9cf4c29a59fb4ab3fd23c1e9308243
Teerrthan118 <94755143+Teerrthan118@users.noreply.github.com>
2022-09-03 09:23:10 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+status = "";
+find = "";
+
+function setup() {
+    canvas = createCanvas(380,380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+}
+
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    find = document.getElementById("object").value;
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+}
+
+function draw() {
+    image(video, 0, 0, 640, 420);
+}
